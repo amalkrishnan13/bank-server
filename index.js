c3489477d3cc991659e4f9601af83e4af67dceda
unknown <amalkrishnan512@gmail.com>
2022-12-22 12:13:49 +0530

    initial

---
+++ index.js
@@ -0,0 +1,116 @@
+// import dataservice file from service folder
+const dataservice = require('./service/dataservice')
+
+// import json web token
+const jwt = require('jsonwebtoken')
+
+// import express
+const express = require('express')
+const { json } = require('express')
+
+// create app
+const app = express()
+
+// to convert json datas
+app.use(express.json())
+
+// middleware for verify the token
+const jwtmiddleware = (req, res, next) => {
+    console.log("router specific middleware..");
+    try {
+        const token = req.headers['access-token']
+        const data = jwt.verify(token, "secretkey123")
+        console.log(data);
+        next()
+    }
+    catch {
+        res.status(422).json({
+            statusCode: 422,
+            status: false,
+            message: "please login"
+        })
+    }
+
+
+}
+
+
+
+
+// requests
+
+//register
+app.post('/register', (req, res) => {
+
+    const result = dataservice.register(req.body.acno, req.body.uname, req.body.psw)
+    res.status(result.statusCode).json(result)
+
+})
+
+//login
+app.post('/login', (req, res) => {
+
+    const result = dataservice.login(req.body.acno, req.body.psw)
+    res.status(result.statusCode).json(result)
+
+})
+//deposit
+app.post('/deposit', jwtmiddleware, (req, res) => {
+
+    const result = dataservice.deposit(req.body.acno, req.body.psw, req.body.amount)
+    res.status(result.statusCode).json(result)
+
+})
+
+//withdraw
+app.post('/withdraw',jwtmiddleware, (req, res) => {
+
+    const result = dataservice.withdraw(req.body.acno, req.body.psw, req.body.amount)
+    res.status(result.statusCode).json(result)
+
+})
+//transaction history
+app.post('/gettransaction',jwtmiddleware, (req, res) => {
+
+    const result = dataservice.gettransaction(req.body.acno)
+    res.status(result.statusCode).json(result)
+
+})
+//delete account
+
+// // get
+
+// app.get('/',(req,res)=>{
+//     res.send('GET method checking......')
+// })
+
+// // post
+// app.post('/',(req,res)=>{
+//     res.send('post method checking......')
+// })
+
+// // put 
+// app.put('/',(req,res)=>{
+//     res.send('put method checking......')
+// })
+
+// // patch
+// app.patch('/',(req,res)=>{
+//     res.send('patch method checking......')
+
+// })
+
+// // delete
+// app.delete('/',(req,res)=>{
+//     res.send('delete method checking......')
+
+// })
+
+
+
+
+
+// set port
+app.listen(3000, () => {
+    console.log('server started at port number 3000');
+})
